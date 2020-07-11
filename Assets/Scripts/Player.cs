f8acf96648caaf049e9ef3cc438df34c834a11bf
Task Nagashige <task.19.xx@gmail.com>
2020-07-11 23:21:42 +0900

    矢印キーで移動できるようにした

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float m_speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      var h = Input.GetAxis("Horizontal");
+      var v = Input.GetAxis("Vertical");
+
+      var velocity = new Vector3(h, v) * m_speed;
+      transform.localPosition += velocity;
+    }
+}
