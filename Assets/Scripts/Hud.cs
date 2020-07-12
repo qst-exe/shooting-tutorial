13ae7bd6eb2ab3788cea1d4203da57cced3731ed
Task Nagashige <task.19.xx@gmail.com>
2020-07-12 17:08:39 +0900

    HPゲージと経験値ゲージを作成した

---
+++ Assets/Scripts/Hud.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hud : MonoBehaviour
+{
+    public Image m_hpGauge;
+    public Image m_expGauge;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var player = Player.m_instance;
+
+        var hp = player.m_hp;
+        var hpMax = player.m_hpMax;
+        m_hpGauge.fillAmount = (float) hp / hpMax;
+
+        var exp = player.m_exp;
+        var prevNeedExp = player.m_prevNeedExp;
+        var needExp = player.m_needExp;
+        m_expGauge.fillAmount = (float) (exp - prevNeedExp) / (needExp - prevNeedExp);
+    }
+}
